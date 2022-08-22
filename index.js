92dca50b4449c76f0a5500d8c43f41b13e0f8212
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-22 20:34:11 +0300

    completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, str) {
+
+  return drivers.filter( driver => driver === str || driver.toLowerCase()===str.toLowerCase() || driver.toUpperCase === str.toUpperCase());
+   
+  }
+function fuzzyMatch(drivers,str){
+console.log(drivers.filter(driver => driver.indexOf(str) === 0))
+return drivers.filter( driver => driver.indexOf(str) === 0);
+}
+
+
+function matchName(driver, str){
+  const driverss = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+  console.log(driverss.filter(item => item.name === "Bobby"))
+  return driverss.filter(item => item.name === "Bobby")
+}
